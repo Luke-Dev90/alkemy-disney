89ff1f48dadcabf6e39b15a20fa351eb0a03946e
Luke-Dev90 <lukee_sf@hotmail.com>
2021-08-24 10:10:51 -0300

    init proyect disney

---
+++ src/main/java/com/lchalela/disnet/api/BackendAlkemyDisneyApplication.java
@@ -0,0 +1,13 @@
+package com.lchalela.disnet.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAlkemyDisneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAlkemyDisneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lchalela/disnet/api/BackendAlkemyDisneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lchalela.disnet.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAlkemyDisneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
