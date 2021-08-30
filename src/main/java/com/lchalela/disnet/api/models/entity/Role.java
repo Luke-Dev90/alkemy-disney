ac130a645b01d96092963e8d0821d8528a08d9fa
Luke-Dev90 <lukee_sf@hotmail.com>
2021-08-30 18:05:57 -0300

    entitys user and role

--- src/main/java/com/lchalela/disnet/api/models/entity/Role.java
+++ src/main/java/com/lchalela/disnet/api/models/entity/Role.java
@@ -2,19 +2,39 @@ package com.lchalela.disnet.api.models.entity;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
-public class Role implements Serializable{
-
-	private static final long serialVersionUID = -4934148390870111203L;
+@Table(name="roles")
+public class Role implements Serializable {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
+	@Column(unique = true, length = 20)
 	private String role;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	private static final long serialVersionUID = -4934148390870111203L;
 }
